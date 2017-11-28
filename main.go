eddb96aa2f12c3aabcee88f2bf77830278d1ced0
pRusya <agat.nk@gmail.com>
2017-11-28 19:29:47 +0200

    Update utils, main, gitignore. Create config.json

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "github.com/asdine/storm"
+import (
+	"github.com/asdine/storm"
+	"os"
+	"github.com/gorilla/mux"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
 
-var db storm.DB
+var db *storm.DB
 
-func main() {}
+type Config struct {
+	Port            string `json:"port"`
+	EveClientId     string `json:"eve_client_id"`
+	EveClientSecret string `json:"eve_client_secret"`
+	EveCallback     string `json:"eve_callback"`
+}
+
+var config Config
+
+func main() {
+	db, err := storm.Open("my.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//In case of cmd args handle them and exit
+	if len(os.Args) > 1 {
+		utilsHandleOptions()
+		return
+	}
+
+	//Read config
+	raw, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(raw, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	http.ListenAndServe(":"+config.Port, r)
+}
--- utils.go
+++ utils.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"log"
+	"flag"
 )
 
-func utilsCreateAdminUser(name sting) error {
+func utilsCreateAdminUser(name string) error {
 	err := dbCreateUser(name, true, true, true)
 	if err != nil {
 		panic(err)
@@ -19,4 +20,18 @@ func utilsListUsers() {
 		panic(err)
 	}
 	log.Printf("%s", users)
+}
+
+func utilsHandleOptions() {
+	flagCreateAdmin := flag.String("createadmin", "",
+		"-createadmin \"<name>\"")
+	flagListUsers := flag.Bool("users", false, "-users")
+	flag.Parse()
+
+	if *flagCreateAdmin != "" {
+		utilsCreateAdminUser(*flagCreateAdmin)
+	}
+	if *flagListUsers {
+		utilsListUsers()
+	}
 }
